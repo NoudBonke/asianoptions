5b40ed9194ce908e3decb072b7f75a11e43d540a
Noud <noudbonke@gmail.com>
2023-06-11 15:49:29 +0200

    add antithetics

--- second_part.cpp
+++ second_part.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <chrono>
 
 #include <eigen3/Eigen/Eigenvalues>
 #include <eigen3/Eigen/Dense>
@@ -25,7 +26,7 @@ void gbm(const double S0, const double r, const double sigma, const double T, co
     return;
 }
 
-double gbm_PCA(const double S0, const double r, const double sigma, const double T, const int N, double * S, Eigen::MatrixXd &PCA){
+double gbm_PCA(const double S0, const double r, const double sigma, const double T, const int N, double * S, double * S_at, Eigen::MatrixXd &PCA){
     double dt = T/(N-1);
     S[0] = S0;
     Eigen::VectorXd normals(N-1);
@@ -37,6 +38,11 @@ double gbm_PCA(const double S0, const double r, const double sigma, const double
     for (auto i=0; i<N-1; ++i){
         S[i+1] = (r - std::pow(sigma, 2)/2)*i*dt + PCAresultEigen[i];
         S[i+1] = S0*std::exp(S[i+1]);
+    }
+    for (auto i=0; i<N-1; ++i){
+        //antithetic by using negative of normals
+        S_at[i+1] = (r - std::pow(sigma, 2)/2)*i*dt - PCAresultEigen[i];
+        S_at[i+1] = S0*std::exp(S_at[i+1]);
     }
     // control variate
     return normals[0];
@@ -86,66 +92,44 @@ void browniancov(const int N, const double T, const double sigma, Eigen::MatrixX
     return;
 }
 
-// void monte_carlo_terminal(double * S, double r, double T, double K, double S0, double sigma,  const int N,  const int N_sims, Eigen::MatrixXd &PCA, double * res, const double cvexpected, const double cvvar){
-//     double S[N];
-//     double cv[N_sims];
-//     for (auto j=0; j<N_sims; ++j){
-//         double dummy = gbm_PCA(S0, r, sigma, T, N, S, PCA);
-//         // collect terminal prices for control variates
-//         cv[j] = S[N-1];
-//         res[j] = std::exp(-r*T)*std::max(mean(S, N) - K, zero);
-//     }
-//     // control variates based on terminal price
-
-//     double cov = covar(res, cv, cvexpected, N_sims);
-//     double bstar = cov/cvvar;
-
-//     double newres[N_sims];
-//     for (auto i=0; i<N_sims; ++i){
-//         newres[i] = res[i] - bstar*(cv[i]-cvexpected);
-//     }
-//     double avg = mean(newres, N_sims);
-//     double error = stdev(newres, N_sims)/std::sqrt(N_sims);
-//     double corr = cov/(sqrt(cvvar)*stdev(res, N_sims));
-//     std::cout << "no variance reduction: " << std::endl;
-//     std::cout << "N = " << N << " : " << mean(res, N_sims) << " +- " << stdev(res, N_sims)/std::sqrt(N_sims) << std::endl;
-//     std::cout << "variance reduction: " << 1 - std::pow(corr, 2) << std::endl;
-//     std::cout << "N = " << N << " : " << avg << " +- " << error << std::endl;
-//     return;
-// }
-
-void monte_carlo_PCA(double r, double T, double K, double S0, double sigma,  const int N,  const int N_sims, Eigen::MatrixXd &PCA, double * res, const double cv_terminal_expected, const double cv_terminal_var, const double cv_PCA_expected, const double cv_PCA_var){
+void monte_carlo_PCA(double r, double T, double K, double S0, double sigma,  const int N,  const int N_sims, Eigen::MatrixXd &PCA, double * res, const double cv_terminal_expected, const double cv_terminal_var, const double cv_PCA_expected, const double cv_PCA_var, const int at){
     double S[N];
-    double cv_PCA[N_sims];
-    double cv_terminal[N_sims];
+    //antithetics
+    double S_at[N];
+    double cv_PCA[(1+at)*N_sims];
+    double cv_terminal[(1+at)*N_sims];
     for (auto j=0; j<N_sims; ++j){
-        cv_PCA[j] = gbm_PCA(S0, r, sigma, T, N, S, PCA);
-        cv_terminal[j] = S[N-1];
-        res[j] = std::exp(-r*T)*std::max(mean(S, N) - K, zero);
+        cv_PCA[(1+at)*j] = gbm_PCA(S0, r, sigma, T, N, S, S_at, PCA);
+        if(at){cv_PCA[(1+at)*j+1] = -cv_PCA[(1+at)*j];}
+
+        cv_terminal[(1+at)*j] = S[N-1];
+        if(at){cv_terminal[(1+at)*j+1] = S_at[N-1];}
+        res[(1+at)*j] = std::exp(-r*T)*std::max(mean(S, N) - K, zero);
+        if(at){res[(1+at)*j+1] = std::exp(-r*T)*std::max(mean(S_at, N) - K, zero);}
     }
     // control variates based on terminal price
 
-    double cov_PCA = covar(res, cv_PCA, cv_PCA_expected, N_sims);
+    double cov_PCA = covar(res, cv_PCA, cv_PCA_expected, N_sims*(1+at));
     double bstar_PCA = cov_PCA/cv_PCA_var;
 
-    double cov_terminal = covar(res, cv_terminal, cv_terminal_expected, N_sims);
+    double cov_terminal = covar(res, cv_terminal, cv_terminal_expected, N_sims*(1+at));
     double bstar_terminal = cov_terminal/cv_terminal_var;
 
-    double newres_PCA[N_sims];
-    double newres_terminal[N_sims];
+    double newres_PCA[N_sims*(1+at)];
+    double newres_terminal[N_sims*(1+at)];
 
-    for (auto i=0; i<N_sims; ++i){
+    for (auto i=0; i<N_sims*(1+at); ++i){
         newres_PCA[i] = res[i] - bstar_PCA*(cv_PCA[i]-cv_PCA_expected);
         newres_terminal[i] = res[i] - bstar_terminal*(cv_terminal[i]-cv_terminal_expected);
     }
-    double avg_PCA = mean(newres_PCA, N_sims);
-    double error_PCA = stdev(newres_PCA, N_sims)/std::sqrt(N_sims);
-    double corr_PCA = cov_PCA/(sqrt(cv_PCA_var)*stdev(res, N_sims));
-    double avg_terminal = mean(newres_terminal, N_sims);
-    double error_terminal = stdev(newres_terminal, N_sims)/std::sqrt(N_sims);
-    double corr_terminal = cov_terminal/(sqrt(cv_terminal_var)*stdev(res, N_sims));
+    double avg_PCA = mean(newres_PCA, N_sims*(1+at));
+    double error_PCA = stdev(newres_PCA, N_sims*(1+at))/std::sqrt(N_sims*(1+at));
+    double corr_PCA = cov_PCA/(sqrt(cv_PCA_var)*stdev(res, N_sims*(1+at)));
+    double avg_terminal = mean(newres_terminal, N_sims*(1+at));
+    double error_terminal = stdev(newres_terminal, N_sims*(1+at))/std::sqrt(N_sims*(1+at));
+    double corr_terminal = cov_terminal/(sqrt(cv_terminal_var)*stdev(res, N_sims*(1+at)));
     std::cout << "no variance reduction: " << std::endl;
-    std::cout << "N = " << N << " : " << mean(res, N_sims) << " +- " << stdev(res, N_sims)/std::sqrt(N_sims) << std::endl;
+    std::cout << "N = " << N << " : " << mean(res, N_sims*(1+at)) << " +- " << stdev(res, N_sims*(1+at))/std::sqrt(N_sims*(1+at)) << std::endl;
     std::cout << "PCA control variate variance reduction: " << 1 - std::pow(corr_PCA, 2) << std::endl;
     std::cout << "N = " << N << " : " << avg_PCA << " +- " << error_PCA << std::endl;
     std::cout << "terminal price control variate variance reduction: " << 1 - std::pow(corr_terminal, 2) << std::endl;
@@ -190,15 +174,19 @@ int main(int argc, char* argv[]){
     Eigen::MatrixXd PCA(N-1,N-1);
     browniancov(N-1, T, sigma, PCA);
 
+    int at = 0;
+    auto start = std::chrono::high_resolution_clock::now();
     for (auto j=3; j<6; ++j){
         int N_sims = std::pow(10, j);
-        double res[N_sims];
+        double res[(1+at)*N_sims];
         std::cout << "\n# of iterations : " << N_sims << std::endl;
         //std::cout << "\nControl variate: first PCA component" << std::endl;
-        monte_carlo_PCA(r, T, K, S0, sigma, N, N_sims, PCA, res, expectedfinalprice, varfinalprice, 0, 1);
+        monte_carlo_PCA(r, T, K, S0, sigma, N, N_sims, PCA, res, expectedfinalprice, varfinalprice, 0, 1, at);
         //std::cout << "Control variate: terminal price" << std::endl;
         //monte_carlo_PCA(r, T, K, S0, sigma, N, N_sims, PCA, res, 0, 1);
     }
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << std::endl;
 
     //writeFile << N << ' ' << res << std::endl;
     writeFile.close();
